841fa6c44d24db50dc63253c9cfe2c522c5c8e46
Kurller <kolaquadry@gmail.com>
2024-01-05 12:25:27 +0100

    first commit

--- new_todo/urls.py
+++ new_todo/urls.py
@@ -22,6 +22,4 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("todoapp.urls"))
-]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
