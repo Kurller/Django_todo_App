1ad516bca23efdac1bbcf46f8f756e07e3c466f9
Kurller <kolaquadry@gmail.com>
2024-01-05 11:08:03 +0100

    first commit

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-
 urlpatterns = [
     path("",views.task_list,name = 'task-list'),
     path("task/<int:pk>/delete",views.task_delete,name = 'task-delete'),
